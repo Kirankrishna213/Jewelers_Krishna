e8bb1a86f75f1a3a34d3ac2f92e741d703a9fb82
Kirankrishna213 <kkirankrishnar@gmail.com>
2025-05-10 22:38:01 +0530

    Update wsgi.py

--- krishna_jewellers/wsgi.py
+++ krishna_jewellers/wsgi.py
@@ -1,1 +1,8 @@
- 
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krishna_jewellers.settings')
+
+# This is the critical WSGI application variable
+application = get_wsgi_application() 
